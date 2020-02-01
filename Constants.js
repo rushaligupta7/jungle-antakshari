04f3aa9ec4f29f4b74ee68cc5cc768a4b3247d4f
rushaligupta7 <56380436+rushaligupta7@users.noreply.github.com>
2020-02-01 16:33:50 +0530

    Update Constants.js

--- Constants.js
+++ Constants.js
@@ -12,38 +12,10 @@ var Constants = /** @class */ (function () {
     Constants.PHONEME = 'phoneme';
     Constants.EMPHASIS = 'emphasis';
     Constants.AMAZON_EFFECT = 'amazon:effect';
+    Constants.INTERJECTION='interjection';
     //Constants.PROSODY
     //Quiz game related messages
-    Constants.MAX_PLAYER_LIMIT_MSG = "आपके पास quiz में minimum 1 और maximum 5 players हो सकते हैं, please बताएं कि कितने players खेल रहे हैं?";
-    Constants.PLAYER_NUM_UNRECOGNISED_MSG = "Sorry, मुझे वह समझ नहीं आया। please बताएं कि कितने players खेल रहे हैं?";
-    Constants.QUIZ_REPLAY_MSG = "चलिए फिर से quiz खेलते है . आप कितने players खेलना चाहते हो?";
-    Constants.QUIZ_STOP_MSG = "Vir Quiz खेलने के लिए धन्यवाद";
-    Constants.YES_OR_NO_MSG = "Please yes या no में से एक कहिये ";
-    Constants.QUIZ_END_MSG = "आपने खेल समाप्त कर लिया है ";
-    Constants.CONGRATULATIONS_MSG = "बधाई हो!   ";
-    Constants.ZERO_WINNER_MSG = "sorry, इस quiz के लिए हमारे पास कोई विजेता नहीं है।!Better luck next time!";
-    Constants.PLAY_AGAIN_MSG = "  क्या आप फिर से game खेलना चाहेंगे?";
-    Constants.NEXT_QUESTION_MSG = "आपके लिए अगला सवाल है ";
-    Constants.TIE_MSG = "के बीच में tie हुई है ";
-    Constants.SCORE_MSG = "और आपका score है ";
-    Constants.ZERO_SCORE_MSG = "और आपका score है zero";
-    Constants.QUESTION_HELP_MSG = 'आपसे पूछे जायेंगे कुछ सवाल , और आपको दिए गए दो options में से कोई एक choose करना होगा.';
-    Constants.OPTION_HELP_MSG = 'आप A. ya B. बोल सकते हो, या सीधे, answer भी कह सकते हो . ';
-    Constants.FALLBACK_MSG = "sorry! मुझे समझ नहीं आया. क्या आप फिर से कहोगे";
-    Constants.NO_IDEA = "no idea";
-    Constants.NUMBER_OF_PLAYERS = "Number_of_Players";
-    Constants.PRINT_PLAYER = "Player: ";
-    Constants.PRINT_SCORE = "Score: ";
-    Constants.WINNER_PLAYER_MSG = "बधाई हो! इस खेल का विजेता है ";
-    Constants.PRINT_TIE_MSG = "Hey! there was a tie!";
-    Constants.OPTION_A = 'a';
-    Constants.OPTION_B = 'b';
-    Constants.A = "A. ";
-    Constants.B = "B. ";
-    //Quiz game related dynamic values
-    Constants.TOTAL_PLAYERS = 5;
-    Constants.score_point = 10;
-    Constants.QUESTIONS_PER_PLAYER = 10;
+  
     //Generic keywords
     Constants.YES = "yes";
     Constants.NO = "no";
@@ -72,24 +44,37 @@ var Constants = /** @class */ (function () {
     //APL related content
     Constants.PRESENTATION__APL = 'Alexa.Presentation.APL';
     Constants.APL_RENDER_DOCUMENT = 'Alexa.Presentation.APL.RenderDocument';
-    Constants.APL_DIRECTIVE_ACTIVE_PLAYER = '../models/imageDesign_Q.json';
-    Constants.APL_DIRECTIVE_WINNER = '../models/imageDesign_winner.json';
+    
     
     //////////////////////////////
     //Quiz game related dynamic values
-    Constants.TOTAL_PLAYERS = 5;
-    Constants.WRONG_RESPONSE=[" ooooo.... you were playing good dude!"," sorry you're out dear"," that was an easy one....you missed it","  better luck next time"," i enjoyed your company"," see you next time"," ohoo..you got it wrong"," aiiyoooo...wrong wrong wrong"," its a wrong one!"];
+    
+    Constants.WRONG_RESPONSE=[" ooooo.... you were playing good dude! "," sorry you're out dear "," that was an easy one....you missed it","  better luck next time"," i enjoyed your company"," see you next time"," ohoo..you got it wrong"," aiiyoooo...wrong wrong wrong"," its a wrong one!"];
     Constants.RESULT_MESSAGE=" time for result now ! your score is ";
-    Constants.END_MESSAGE=" thank you for playing!";
-    Constants.START_PROMPTS=["let me think something ,mmmmm ", " it can be ","i will say "];
-    Constants.SKIP_PROMPTS=["hmmm, let me try ", " ok i will go with ","yay , i know , its a "];
-    Constants.REPROMT_MESSAGE1=["tick tok tick.....fast "," time is running a marathon ", "come on come on  ", " hurry up partner ", " do not overthink "," you can do it "," go ahead "];
-    Constants.REPROMT_MESSAGE2=["start with ","last letter was ","say with ", " try with ","you have to start with "," find the one starting with "];
+    Constants.END_MESSAGE=" thank you for playing! ";
+    Constants.START_PROMPTS=[" let me think something ,mmmmm ", " it can be ","i will say "];
+    Constants.SKIP_PROMPTS=[" hmmm, let me try ", " ok i will go with ","yay , i know , its a "];
+    Constants.REPROMT_MESSAGE1=[" tick tick tick.....fast "," time is running a marathon ", "come on come on  ", " hurry up partner ", " do not overthink "," you can do it "," go ahead "];
+    Constants.REPROMT_MESSAGE2=[" start with "," last letter was "," say with ", " try with "," you have to start with "," find the one starting with "];
+    Constants.FIRST_WORD_MESSAGE=[" so lets start with "," lets get started with "," i will start with "," lets start . the first word is "];
+    Constants.DUPLICATE_MESSAGE=[" this was used earlier ", " you can't repeat this again "," no cheating haa , this word is taken "];
+    Constants.REPLAY_MESSAGE=" lets play the game again! the first word is ";
+    Constants.BYE_MESSAGE=" nice playing with you! see you again! ";
+    Constants.INVALID=" this animal or bird hopefully does not exist! ";
+    Constants.WRONG=[" this does not start with ", " it should start with ","please start with ", " it must start with "];
+    Constants.DISPLAY_LAST_WORD=" Enjoy Playing ! Last word was : ";
     
+    ////////////////////
+    //urls of audio_files
+    Constants.SKIP_AUDIO=["https://wordantakshari.s3.amazonaws.com/skip1.mp3","https://wordantakshari.s3.amazonaws.com/skip2.mp3","https://wordantakshari.s3.amazonaws.com/skip3.mp3","https://wordantakshari.s3.amazonaws.com/skip4.mp3"];
+    Constants.INTRO_AUDIO=["https://wordantakshari.s3.amazonaws.com/score1.mp3"];
+    Constants.SCORE_AUDIO=["https://wordantakshari.s3.amazonaws.com/score1.mp3"];
+    Constants.POP_AUDIO="https://wordantakshari.s3.amazonaws.com/popsound.mp3";
+    Constants.SPEECHCONS=["ah ","all righty ","awesome ","balle balle ","bang ","bingo ","bravo ","checkmate ","hurray ","jinx ","ooh la la ","righto ","shabash ","waah "];
+    Constants.SKIP_CONS=["aii ","aiyo ","arrey ","aw ","dishoom "];
+    Constants.SCORE_CONS=["beep beep ","meow ","well well "];
     
-    
-    
-    
+
     
     return Constants;
 }());
